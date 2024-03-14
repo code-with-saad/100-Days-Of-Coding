b11680a8165cf158d0c5201d7735a5f413828136
unknown <saadkashif770@gmail.com>
2024-03-15 01:36:53 +0500

    Day 1-Question1

---
+++ Day 1/Question1.js
@@ -0,0 +1,2 @@
+var userName = "Saad";
+console.log("Hello ".concat(userName, ", would you like to learn some TypeScript today?"));
---
+++ Day 1/Question1.ts
@@ -0,0 +1,3 @@
+let userName:string = "Saad";
+
+console.log(`Hello ${userName}, would you like to learn some TypeScript today?`)
